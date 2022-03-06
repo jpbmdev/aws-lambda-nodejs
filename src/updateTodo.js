df30aad67f0e1180baa3b1e436002267ee5aa3b5
jpbmdev <jpbmdev@gmail.com>
2022-03-06 18:47:41 -0500

    :tada: proyect created

---
+++ src/updateTodo.js
@@ -0,0 +1,32 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+const updateTodo = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { completed } = JSON.parse(event.body);
+  const { id } = event.pathParameters;
+
+  await dynamodb
+    .update({
+      TableName: "TodoTable",
+      Key: { id },
+      UpdateExpression: "set completed = :completed",
+      ExpressionAttributeValues: {
+        ":completed": completed,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Todo updated",
+    }),
+  };
+};
+
+module.exports = {
+  handler: updateTodo,
+};
